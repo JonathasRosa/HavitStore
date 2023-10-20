96f804c18ae60d4517f524cd44ed210cc05c1f71
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-10-20 11:39:31 -0300

    Update HavitStore
    Feat: add auth hander to header component and images in assets.

--- vs-0.1/src/components/ui/header.tsx
+++ vs-0.1/src/components/ui/header.tsx
@@ -18,35 +18,49 @@ import {
     SheetTrigger 
 } from "./sheet";
 import { signIn, useSession, signOut } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar"
+import { Separator } from "./separator"
+
 
 function Header() {
     const { status, data } = useSession();
 
     const handleLoginClick = async () => {
-        await signIn();
-    };
+        await signIn()
+    }
 
     const handleLogoutClick = async () => {
-        await signOut();
-    };
+        await signOut()
+    }
 
     return (
-        <Card className="flex items-center justify-between p-[1.875rem">
+        <Card className="flex items-center justify-between p-[1.875rem]">
             <Sheet>
                 <SheetTrigger asChild>
                     <Button size="icon" variant="outline">
                         <MenuIcon />
                     </Button>
                 </SheetTrigger>
-
                 <SheetContent className="w-[300px] sm:w-[450px]" side="left">
                     <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
                     {status === 'authenticated' && data?.user && (
-                        <Avatar>
-
-                        </Avatar>
+                        <div className="flex flex-col">
+                            <div className="flex items-center gap-2 py-4">
+                            <Avatar>
+                                <AvatarFallback>
+                                {data.user.name?.[0].toUpperCase()}
+                                </AvatarFallback>
+                                {data.user.image && <AvatarImage src={data.user.image} />}
+                                </Avatar>
+                                <div className="flex flex-col">
+                                    <p className="font-bold">{data.user.name}</p>
+                                    <p className="text-sm opacity-75">Boas compras!</p>
+                                </div>
+                            </div>
+                            <Separator />
+                        </div>
                     )}
-                    <div className="mt-2 flex flex-col gap-2">
+                    <div className="mt-4 flex flex-col gap-2">
                         {status === 'unauthenticated' && (
                             <Button onClick={handleLoginClick} variant="outline" className="w-full justify-start gap-2">
                                 <LogInIcon size={16} />
@@ -74,11 +88,9 @@ function Header() {
                     </div>
                 </SheetContent>
             </Sheet>
-
             <h1 className="text-lg font-semibold">
                 <span className="text-primary">Havit</span>Store
             </h1>
-
             <Button size="icon" variant="outline">
                 <ShoppingCartIcon />
             </Button>
@@ -87,7 +99,3 @@ function Header() {
 }
 
 export default Header;
-
-function signOut() {
-    throw new Error("Function not implemented.");
-}

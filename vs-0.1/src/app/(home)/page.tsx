03726735bc49a40c76eedda7843bf8763ad8f35b
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-10-20 16:12:49 -0300

    feat: add Categories and CategoriesItem component

--- vs-0.1/src/app/(home)/page.tsx
+++ vs-0.1/src/app/(home)/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
     <div className="p-5"> 
       <Image 
-        src="/banner-55%off.png"
+        src="/banner55off.png"
         height={0}
         width={0}
         className="h-auto w-full"
         sizes="100vw"
         alt={"Até 55% de desconto esse mês!"} />
+        <div className="mt-8">
+        <Categories />
+        </div>
     </div>
   )
 }

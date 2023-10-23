d7366dfec5484fff1627dc7b3015512c355188c8
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-10-23 09:14:35 -0300

    Update  HaviTechStore

--- vs-0.1/src/app/(home)/components/product-list.tsx
+++ vs-0.1/src/app/(home)/components/product-list.tsx
@@ -1,13 +1,17 @@
 import ProductItem from "@/components/ui/product-item";
+import { computerProductTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
 
 interface ProductListProps {
-    products: Product[];
+    product: Product[];
 }
 
-const ProductList = ({ products }: ProductListProps) => {
-    return (<div className="flex w-full gap-4 overflow-x-auto px-5">
-        {products.map(product => <ProductItem  key={product.id} product={products} />)}
+const ProductList = ({product}: ProductListProps) => {
+    return (
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&:: -webkit-scrollbar]:hidden">
+        {product.map((product) => (
+            <ProductItem  key={product.id} product={computerProductTotalPrice(product)} />
+        ))}
         </div>
     );
 }
--- vs-0.1/src/components/ui/product-item.tsx
+++ vs-0.1/src/components/ui/product-item.tsx
@@ -1,15 +1,42 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
+import Image from "next/image";
 
 interface ProductItemProps {
-    product: Product[]
+    product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <div>
-            <h1>Product</h1>
+        <div className="flex max-w-[156px] flex-col gap-4">
+            <div className="flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+                <Image
+                    src={product.imageUrls[0]}
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="h-auto max-h-[70%] w-auto max-w-[80%] "
+                    style={{ objectFit: "contain" }}
+                    priority={true}
+                    alt={product.name}
+                />
+            </div>
+            <div>
+                <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+                    {product.name}
+                </p>
+                <div className="flex items-center gap-2">
+                    {product.discountPercentage> 0 ? (
+                        <>
+                            <p className="font-semibold text-sm">R$ {product.totalPrice.toFixed(2)}</p>
+                            <p className="opacity-75 line-through text-xs">R$ {Number(product.basePrice).toFixed(2)}</p>
+                        </>
+                    ) : (
+                        <p className="font-semibold text-sm">R$ {product.basePrice.toFixed(2)}</p>
+                    )}
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default ProductItem;

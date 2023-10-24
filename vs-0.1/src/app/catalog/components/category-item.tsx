973b49a73a234de40a173b493b59f9d74e582a41
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-10-24 10:10:05 -0300

    feat: add Category-Item Component and Footer text-center

---
+++ vs-0.1/src/app/catalog/components/category-item.tsx
@@ -0,0 +1,33 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategoryItemProps {
+    category: Category
+}
+
+const CategoryItem = ({category}: CategoryItemProps) => {
+    return (
+        <div className="flex flex-col">
+            <div className=" flex w-full h-[150px] items-center justify-center 
+            bg-gradient-to-r from-[#5033C3] to-indigo-600 rounded-tl-lg rounded-tr-lg">
+                <Image 
+                    src={category.imageUrl}
+                    alt={category.name}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    priority={true}
+                    className="h-auto max-h-[70%] w-auto max-w-[80%]"
+                    style={{
+                        objectFit: "contain",
+                    }}
+                />
+            </div>
+            <div className="bg-accent py-3 rounded-br-lg rounded-bl-lg">
+                <p className="text-sm text-center font-semibold">{category.name}</p>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryItem;
--- vs-0.1/src/components/ui/footer.tsx
+++ vs-0.1/src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+        <div className="bg-accent px-8 py-4 text-[0.625rem] text-center">
             @ 2023 Copyright <span className="semibold text-primary">HaviTech</span>Store</div>
     )
 }

e90098a6fb3d016ea5b8613a5ae2bfa5ea28c547
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-10-27 19:20:39 -0300

    feat: ensure product can be added to car

--- vs-0.1/src/components/ui/header.tsx
+++ vs-0.1/src/components/ui/header.tsx
@@ -22,6 +22,7 @@ import { signIn, useSession, signOut } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar"
 import { Separator } from "./separator"
 import Link from "next/link";
+import Cart from "./cart";
 
 
 function Header() {
@@ -103,9 +104,16 @@ function Header() {
                     <span className="text-primary">HaviTech</span>Store
                 </h1>
             </Link>
-            <Button size="icon" variant="outline">
-                <ShoppingCartIcon />
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <ShoppingCartIcon />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent>
+                    <Cart />
+                </SheetContent>
+            </Sheet>
         </Card>
     );
 }
